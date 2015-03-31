368275d146850a5e168409a71d9fe1a04c237ead
Lee Ho Sung <lee.ho.sung@gmail.com>
2015-03-31 19:18:30 +0900

    parse ChildFacilityItemResponse

---
+++ facility.py
@@ -0,0 +1,58 @@
+from childcare_service_api import ChildcareServiceApi
+from datetime import datetime
+
+class Facility(object):
+
+    def __init__(self, facility_id):
+        self.facility_id = facility_id
+        self.cs_api = ChildcareServiceApi()
+
+    @staticmethod
+    def crawl_facilities():
+        yield
+
+    def crawl_facility_info(self):
+        response = self.cs_api.get_child_facility_item(self.facility_id)
+
+        # ChildFacilityInfo
+        info = response.ChildFacilityInfo
+        self.facility_name = info.FacilityName
+        self.cr_type = info.CRType
+        self.cr_spec = info.CRSpec
+        self.fixed_number = int(info.FixedNumber)
+        self.present_number = int(info.PresentNumber)
+        self.president_name = info.PresidentName
+        self.open_date = datetime.strptime(info.OpenDate, "%Y-%m-%d")
+        self.vehicle = True if info.Vehicle == 'Y' else False
+        self.telephone = info.Telephone
+        self.address = ' '.join(info.Address.split())
+        self.gov_support = True if info.GovSupport == 'Y' else False
+        self.accident_insurance = True if info.AccientInsurance == 'Y' else False
+        self.fire_insurance = True if info.FireInsurance == 'Y' else False
+        self.compensation_insurance = True if info.CompensationInsurance == 'Y' else False
+
+        # ChildcarePrice
+        prices = response.ChildcarePrice
+        self.prices = dict()
+        for k, v in prices:
+            k = k.replace("Age", "").lower()
+            self.prices[k] = int(v)
+
+        # ChildFacilityWaiting
+        child_facility_waiting = response.ChildFacilityWaiting
+        self.waitings = dict()
+        for k, v in child_facility_waiting:
+            if k.startswith("Age"):
+                k = k.replace("Age", "").lower()
+                self.waitings[k] = int(v)
+        self.reserv_confirm = child_facility_waiting.reservConfirm
+        self.reserv_wait = child_facility_waiting.reservWait
+        self.reserv_remain = child_facility_waiting.reservRemain
+        self.reserv_regular = child_facility_waiting.reservRegular
+
+        # ChildFacilityPhoto
+        photos = response.ChildFacilityPhoto
+        self.photos = []
+        for k, v in photos:
+            self.photos.append(v)
+
---
+++ tests/test_facility.py
@@ -0,0 +1,59 @@
+import unittest
+from datetime import datetime
+import sys
+sys.path.insert(0, '..')
+import os
+from facility import *
+
+
+class FacilityTestCase(unittest.TestCase):
+
+    def test_crawl_facilities(self):
+        facilities = [x for x in Facility.crawl_facilities()]
+
+    def test_crawl_facility_info(self):
+        f = Facility(11215000106)
+        f.crawl_facility_info()
+        self.assertEqual("새싹어린이집", f.facility_name)
+        self.assertEqual("민간개인", f.cr_type)
+        self.assertEqual("일반", f.cr_spec)
+        self.assertTrue(hasattr(f, "fixed_number"))
+        self.assertTrue(hasattr(f, "present_number"))
+        self.assertEqual("박현정", f.president_name)
+        self.assertEqual(datetime(2001, 5, 21), f.open_date)
+        self.assertEqual(True, f.vehicle)
+        self.assertEqual("02-457-7702", f.telephone)
+        self.assertEqual("서울 광진구 자양2동 682-4 1층", f.address)
+        self.assertEqual(False, f.gov_support)
+        self.assertEqual(True, f.accident_insurance)
+        self.assertEqual(True, f.fire_insurance)
+        self.assertEqual(True, f.compensation_insurance)
+
+        self.assertEqual({
+            "0": 383300,
+            "1": 337700,
+            "2": 278000,
+            "3": 243000,
+            "4": 238000,
+            "5over": 238000
+        }, f.prices)
+
+        self.assertEqual({
+            "0": 1,
+            "1": 0,
+            "2": 2,
+            "3": 0,
+            "4": 0,
+            "5over": 0
+        }, f.waitings)
+
+        self.assertEqual(0, f.reserv_confirm)
+        self.assertEqual(0, f.reserv_wait)
+        self.assertEqual(0, f.reserv_remain)
+        self.assertEqual(0, f.reserv_regular)
+
+        self.assertEqual(5, len(f.photos))
+
+
+if __name__ == '__main__':
+    unittest.main()
