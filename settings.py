28b60a038a792206a860146bd802fa6a526e0601
Lee Ho Sung <lee.ho.sung@gmail.com>
2015-04-06 10:22:59 +0900

    hide api key configurations

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-import logging
+import os
+import configparser
 
 ARNAMES = [
     "서울특별시", "부산광역시", "대구광역시",
@@ -8,16 +9,9 @@ ARNAMES = [
     "경상북도", "경상남도", "제주도"
 ]
 
-API020_KEY = "e52250c1fe194b0fbed49dced4d20376"
-DAUM_KEY = "785e8fe5def74896cc2d2c4ace066d4d"
-CHILDCARE_SERVICE_KEY = \
-    "4COh4sQ6iwph0lr3lsHdJjuSYuXFXtN6phc1+m2ALWgDfC9rGAFyLkwU/" \
-    "R3pDYm/jMPuzqmS6a6NJDx1oPHU0w=="
+DAUM_KEY = os.environ.get("DAUM_KEY")
+API020_KEY = os.environ.get("API020_KEY")
+CHILDCARE_SERVICE_KEY = os.environ.get("CHILDCARE_SERVICE_KEY")
 
-DB_FILENAME = "nursery.json"
-
-CONSOLE_LOG_LEVEL = logging.DEBUG
-FILE_LOG_LEVEL = logging.DEBUG
-
-LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-LOG_FILENAME = 'nursery-crawl.log'
+if os.path.isfile("local_settings.py"):
+    from local_settings import *
